b77cf1884e68d88267c6f8177620557f1561eeb7
RamSChoudhary <ramrit10@gmail.com>
2026-09-14 21:41:57 +0530

    Initial commit

--- agents/sqlanalyst.py
+++ agents/sqlanalyst.py
@@ -2,6 +2,18 @@ import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))  # Add the parent directory to sys.path
 from utils import llm_pick
+from models import AgentSchema
 
-llm_obj=llm_pick.pick_llm("low")
-print(llm_obj.invoke("What is the capital of France?"))  # Output: gpt-5.6-luna
+def curate_question(state: AgentSchema) -> AgentSchema:
+    user_question = state.user_question
+    llm = llm_pick.pick_llm("low")  # Pick the appropriate LLM based on the level of complexity
+    response = llm.invoke(f"Curate the following user question: {user_question}")
+    state.curated_ques = response
+    return state
+
+def prompt_query_context(state: AgentSchema) -> AgentSchema:
+    curated_ques = state.curated_ques
+    llm = llm_pick.pick_llm("medium")  # Pick the appropriate LLM based on the level of complexity
+    response = llm.invoke(f"Generate a detailed prompt with SQL DB context for the following curated question: {curated_ques}")
+    state.prompt_query_context = response
+    return state
